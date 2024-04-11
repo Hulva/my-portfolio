c23900cc82e4a3e3c2735020135310d201caaeb7
Luva hua <lvsweet217@163.com>
2024-04-11 23:44:37 +0800

    chore: update ackee tracker data-ackee-opts

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="zh-cn">
       <head>
-        <script async src="https://74.120.173.186/ackee/tracker.js" data-ackee-server="https://74.120.173.186/ackee/" data-ackee-domain-id="3d8ff296-b274-42af-a190-4e90acfee9cd" data-ackee-opts='{ "ignoreLocalhost": true, "detailed": true }'></script>
+        <script async src="https://74.120.173.186/ackee/tracker.js"
+          data-ackee-server="https://74.120.173.186/ackee/"
+          data-ackee-domain-id="3d8ff296-b274-42af-a190-4e90acfee9cd"
+          data-ackee-opts='{ "ignoreLocalhost": true, "detailed": true, "ignoreOwnVisits": true }'></script>
       </head>
       <body className={inter.className}>
         <ToastContainer />
